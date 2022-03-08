7b3dc4edb927ddb1fce9046b08bcea21dbbca41c
LUKAS SANTOS VENANCIO <21276375@cfp127aluno.local>
2022-03-08 16:55:43 -0300

    Adding to the github.

---
+++ app/src/main/java/br/senai/jogodavelha/MainActivity.kt
@@ -0,0 +1,19 @@
+package br.senai.jogodavelha
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val start = findViewById<Button>(R.id.start)
+
+        start.setOnClickListener(){
+            
+        }
+
+    }
+}
