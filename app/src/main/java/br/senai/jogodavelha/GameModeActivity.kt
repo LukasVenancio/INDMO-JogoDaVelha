edda4b21954959a527959d533289ce1f2b816864
LUKAS SANTOS VENANCIO <21276375@cfp127aluno.local>
2022-03-22 16:57:28 -0300

    Faltando Game Activity.

--- app/src/main/java/br/senai/jogodavelha/GameModeActivity.kt
+++ app/src/main/java/br/senai/jogodavelha/GameModeActivity.kt
@@ -1,11 +1,31 @@
 package br.senai.jogodavelha
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.senai.jogodavelha.databinding.ActivityGameModeBinding
 
 class GameModeActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityGameModeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_game_mode)
+        binding = ActivityGameModeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.player.setOnClickListener(){
+
+            binding.machine.background = getDrawable(R.drawable.botao_sem_preenchimento)
+
+            val intent = Intent(this, SelectIconActivity::class.java)
+            startActivity(intent)
+
+        }
+
+        binding.machine.setOnClickListener(){
+
+        }
     }
 }
---
+++ app/src/main/java/br/senai/jogodavelha/SelectIconActivity.kt
@@ -0,0 +1,11 @@
+package br.senai.jogodavelha
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectIconActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_icon)
+    }
+}
